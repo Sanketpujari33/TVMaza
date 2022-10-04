56a025389580b6ea196421b4fa1a0f153182263c
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2022-10-04 20:50:37 +0530

    Update nevbar.js

--- src/Components/nevbar.js
+++ src/Components/nevbar.js
@@ -20,26 +20,12 @@ const Nevbar = () => {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">TVmaza</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    {/* <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link"  href="#">Home</a>
-                            </li>
-                        </ul>
-                        </div> */}
-                        <form className="d-flex">
+                        <form className="d-flex" role="search">
                             <div class="form-check form-check-inline">
                                     <input type="checkbox" class="btn-check" id="btn-check-3" value={selected}  checked={selected === true} onChange={handleChange} />
                                     <label class="btn btn-primary" for="btn-check-3">{selectValue}</label>
                             </div>
-                            {/* <div class="form-check form-check-inline">
-                                    <input type="checkbox" class="btn-check" id="btn-check-3"  value={selected} checked={selected === false} onChange={handleChange} />
-                                    <label class="btn btn-primary" for="btn-check-3">Shows</label>
-                            </div> */}
-                        </form>
+                    </form>
                 </div>
             </nav>
         </div>
